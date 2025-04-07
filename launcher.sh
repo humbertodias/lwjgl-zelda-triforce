ca2153e34ab16a0c4224fb7aa7c3471d74550ef7
Humberto Dias <hldtux@gmail.com>
2025-04-07 15:23:20 -0300

    launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-
-TAG_NAME=$(curl -s https://api.github.com/repos/humbertodias/lwjgl-zelda-triforce/tags | jq -r '.[0].name')
-echo "Version: $TAG_NAME"
-
-if [ ! -d "natives" ]; then
-    echo "Downloading natives.zip"
-    curl -L https://github.com/humbertodias/lwjgl-zelda-triforce/releases/download/$TAG_NAME/natives.zip -o natives.zip
-    unzip natives.zip && rm natives.zip
-fi
+cd "${BASH_SOURCE%/*}" || exit
 
 JAVA_OPTS="-Djava.library.path=natives"
-OS="$(uname -s)"
-if [ "$OS" = "Darwin" ]; then
-  JAVA_OPTS="$JAVA_OPTS -XstartOnFirstThread"
-fi
-echo "JAVA_OPTS=$JAVA_OPTS"
+[ "$(uname -s)" = "Darwin" ] && JAVA_OPTS+=" -XstartOnFirstThread"
 
-java "$JAVA_OPTS" -jar triforce-lwjgl.jar
+exec java $JAVA_OPTS -jar triforce-lwjgl.jar
